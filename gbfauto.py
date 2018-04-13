1b332ed21e1d348742528d3409ee694544b0b32f
xianhaic <xianhaic@gmail.com>
2018-04-13 06:39:12 -0700

    added more to general functions

--- gbfauto.py
+++ gbfauto.py
@@ -1,5 +1,6 @@
 from selenium.common.exceptions import NoSuchElementException
 import time
+import random
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -13,7 +14,8 @@ from selenium.common.exceptions import WebDriverException
 
 RESIZE = "0.99"
  
-def click_by(driver, ref, by, name, locate_delay=3, click_delay=1):
+def click_by(driver, ref, by, name, locate_delay=3, click_delay=1,
+        random_time=True):
     print("Attempting to retrive:", name)
     while True:
         try:
@@ -36,10 +38,10 @@ def click_by(driver, ref, by, name, locate_delay=3, click_delay=1):
         except NoSuchWindowException:
             driver.switch_to_window(driver.window_handles[0])
             print("Retrying:", name, "NoSuchWindowException")
-            time.sleep(locate_delay)
-            continue
  
     print(element.location)
+    if (random_time):
+        time.sleep(random.uniform(0.2, 2))
     click_retry(driver, element)
  
 
@@ -55,16 +57,17 @@ def get_element_by(driver, ref, by, name, locate_delay=3):
             return element
         except NoSuchElementException:
             print("Retrying:", name, "NoSuchElementException")
+            time.sleep(locate_delay)
             continue
         except TimeoutException:
             print("Retrying:", name, "TimeoutException")
+            time.sleep(locate_delay)
             continue
 
 
 def click_retry(driver, element, retry_delay=3, click_delay=1, force=False):
     while True:
         try:
-            time.sleep(click_delay)
             webdriver.ActionChains(driver).move_to_element(element).perform()
             webdriver.ActionChains(driver).send_keys(Keys.DOWN).perform()
 
@@ -110,7 +113,7 @@ def wait_for_pattern(driver, pattern, delay=3):
             break
         time.sleep(delay)
 
-def wait_for_displayed(driver, by, name):
+def wait_for_displayed(driver, by, name, delay=3):
     while True:
         print("Waiting for:", name)
         try:
@@ -120,6 +123,7 @@ def wait_for_displayed(driver, by, name):
                 print("Done waiting")
         except:
             pass
+        time.sleep(delay)
 
 
 
@@ -127,43 +131,76 @@ def load_wait(driver, url, delay=3):
     driver.get(url)
     time.sleep(delay)
 
-def enter_quest(driver, url, element, summons):
+def select_summon(driver, summons):
+    
+    
+    # get divs that contain summons
+    attributes = driver.find_elements(By.CLASS_NAME, "prt-supporter-attribute")
+
+    found = False
+    
+    # find preferred summon
+    for summon in summons:
+        for i in range(len(attributes)):
+            # find summon elements
+            summons_found = attributes[i].find_elements(By.CSS_SELECTOR,
+                'img[alt="' + str(summon) + '"]')
+            
+            if len(summons_found) != 0:
+                
+                # click the given color element
+                if (i == 7):
+                    click_by(driver, driver, By.CSS_SELECTOR, 'div[data-type="'
+                            + str(0) + '"]')
+                    
+                else:
+                    click_by(driver, driver, By.CSS_SELECTOR, 'div[data-type="'
+                            + str(i) + '"]')
+                
+                click_retry(driver, summons_found[0], force=True)
+                found = True
+                break
+            
+            # check next summon if there's none of this one
+            else:
+                continue
+
+        if found:
+            break
+
+    # click default summon
+    if not found:
+        click_retry(driver,
+                attributes[element].find_elements(By.CLASS_NAME,
+                "prt-button-cover")[0])
+
+# returns a list of visible popups, length zero if none
+def checkfor_popups(driver, popup_type="NONE"):
+    if popup_type == "NONE":
+        popups = driver.find_elements(By.CLASS_NAME, "pop-usual")
+    else:
+        popups = driver.find_elements(By.CLASS_NAME, popup_type)
+    
+    popups_visible = []
+
+    for popup in popups:
+        if "display: block;" in popup.get_attribute("style"):
+            popups_visible.append(popup)
+    return popups_visible
+
+
+# returns a list of popups upon fail, an empty list otherwise
+def enter_quest(driver, url, summons):
     
     while True:
         # load into the quest support page
         load_wait(driver, url)
         
-        # get divs that contain summons
-        attributes = driver.find_elements(By.CLASS_NAME, "prt-supporter-attribute")
-        
-        # click the given color element
-        click_by(driver, driver, By.CSS_SELECTOR, 'div[data-type="' + str(element) + '"]')
-        
-        found = False
-        
-        # find preferred summon
-        for summon in summons:
-            # find summon elements
-            summons_found = attributes[element].find_elements(By.CSS_SELECTOR,
-                    'img[alt="' + str(summon) + '"]')
-            
-            # check next summon if there's none of this one
-            if len(summons_found) == 0:
-                continue
-            else:
-                click_retry(driver, summons_found[0], force=True)
-                found = True
-                break
-        # click default summon
-        if not found:
-            click_retry(driver,
-                    attributes[element].find_elements(By.CLASS_NAME,
-                    "prt-button-cover")[0])
+        select_summon(driver, summons)
 
         # wait for popup
         while True:
-            confirmation = get_element_by(driver, driver, By.CSS_SELECTOR,
-                    'div[class="pop-deck supporter"]')
+            confirmation = get_element_by(driver, driver, By.CLASS_NAME, "pop-deck")
             if "display" in confirmation.get_attribute("style"):
                 print("break")
                 break
@@ -186,7 +223,16 @@ def enter_quest(driver, url, element, summons):
         button_ok = confirmation.find_element(By.CLASS_NAME, "btn-usual-ok")
         click_retry(driver, button_ok)
         
+        time.sleep(2)
+
+        popups_visible = checkfor_popups(driver)
+
+        if len(popups_visible) != 0:
+            return popups_visible
+
         break
+
+    return []
 
 
 def fill_ap(driver, num_pots=1, finish_delay=3):
@@ -194,8 +240,15 @@ def fill_ap(driver, num_pots=1, finish_delay=3):
     
     load_wait(driver, url)
     
-    click_by(driver, driver, By.CSS_SELECTOR, 'div[class="btn-item-tabsitems"]')
+    click_by(driver, driver, By.CSS_SELECTOR, 'div[class="btn-item-tabs items"]')
 
+    '''
+    items = WebDriverWait(driver, 3).until(
+       EC.presence_of_element_located((By.CSS_SELECTOR,
+       'div[class="btn-item-tabs items active"]'))
+    )
+    '''
+    time.sleep(5)
     click_by(driver, driver, By.CSS_SELECTOR, 'div[class="lis-item se"][data-index="1"]')
     
     # select number of pots to consume
@@ -220,13 +273,15 @@ def click_char(driver, char_index):
 
 def activate_skill(driver, char_index, skill_list):
     # retrieve party element
-    party = get_element_by(driver, driver, By.CLASS_NAME, "prt-member")
-    
-    # retrive char relative to party element
-    char = get_element_by(driver, party, By.CSS_SELECTOR, 'div[class="lis-character' +
-            str(char_index) + ' btn-command-character"]')
+    party = get_element_by(driver, driver, By.CLASS_NAME, "prt-member",
+            locate_delay=0.5)
     
     click_char(driver, char_index)
+
+    # retrive char relative to party element (index is +1)
+    char = get_element_by(driver, driver, By.CSS_SELECTOR,
+            'div[class="prt-command-chara chara' +
+            str(char_index + 1) + '"]', locate_delay=0.5)
 
     # click on char's skills
     for skill in skill_list:
@@ -234,7 +289,7 @@ def activate_skill(driver, char_index, skill_list):
         '"]')
     
     # click back button
-    click_by(driver, driver, By.CSS_SELECTOR, 'div[class="btn-command-back display-off display-on"]')
+    click_by(driver, driver, By.CLASS_NAME, "btn-command-back")
 
 def attack(driver, auto=False):
     # click attack button
@@ -249,11 +304,13 @@ def attack(driver, auto=False):
         auto = get_element_by(driver, driver, By.CSS_SELECTOR, 
                 'div[class="btn-auto"]')
         if auto.get_attribute("style") != "display: none;":
+            print("auto visible")
             break
-        time.sleep(1)
+        time.sleep(0.3)
 
+    time.sleep(1)
     click_by(driver, driver, By.CSS_SELECTOR, 
-                'div[class="btn-auto"]')
+                'div[class="btn-auto"]', random_time=False)
 
 def test_sequence(driver):
     enter_quest(driver,
@@ -288,14 +345,123 @@ def test_sequence(driver):
                 'div[class="btn-auto"]')
 
 
-def robomi_sequence(driver):
-    enter_quest(driver,
-            'http://game.granbluefantasy.jp/#quest/supporter/727281/3', 6,
-            [2040003000])
+def quest_sequence(driver, time_hrs=1.0, delay_sequence=5):
+    time_s = 1.0 * 60 * 60
+    time_start = time.time()
 
-    wait_for_displayed(driver, By.CSS_SELECTOR, 'div[class="btn-attack-start display-on"]')
+    while time.time() < time_start + time_s:
+        enter_quest(driver,
+                'http://game.granbluefantasy.jp/#quest/supporter/728111/5', [2040003000])
+
+        wait_for_displayed(driver, By.CSS_SELECTOR, 'div[class="btn-attack-start display-on"]')
+        
+        activate_skill(driver, 1, [4003])
+        activate_skill(driver, 2, [1021, 36])
+
+        attack(driver, auto=True)
+
+        while True:
+            if "result" in driver.current_url:
+                time.sleep(delay_sequence)
+                break
+            time.sleep(3)
+        
+
+def get_raid_id(driver, raid_options, refresh_delay=3):
+    url_raid = "https://www.gbfraiders.com/?"
+    url_complete = url_raid
     
-    activate_skill(driver, 1, [1098, 5445])
-    activate_skill(driver, 3, [4003])
+    for i in range(len(raid_options)):
+        url_complete += "raid=" + raid_options[i]
+        if i != len(raid_options) - 1:
+            url += "&"
 
-    attack(driver, auto=True)
+    create_raid_tab = True
+
+    for handle in driver.window_handles:
+        driver.switch_to_window(handle)
+        if "gbfraiders" not in driver.current_url:
+            continue
+        create_raid_tab = False
+        break
+
+    if create_raid_tab:
+        driver.execute_script("window.open()")
+        print "new tab"
+        driver.switch_to_window(driver.window_handles[-1])
+        driver.get(url_complete)
+    elif driver.current_url != url_complete:
+        driver.get(url_complete)
+
+    time.sleep(refresh_delay)
+
+    selected_raid = get_element_by(driver, driver, By.CLASS_NAME,
+            "copy-div")
+    
+    raid_id = selected_raid.get_attribute("id")        
+    driver.switch_to_window(driver.window_handles[0])
+    return raid_id
+
+def clear_pending_battles(driver):
+    url_unclaimed = "http://game.granbluefantasy.jp/#quest/assist/unclaimed"
+    
+    raids_unclaimed = True
+    
+    while raids_unclaimed:
+
+        load_wait(driver, url_unclaimed)
+        raids = driver.find_elements(By.CSS_SELECTOR, 
+                'div[class="btn-multi-raid lis-raid"]')
+        if len(raids) == 0:
+            raids_unclaimed = False
+        else:
+            click_by(driver, raids[0], By.CLASS_NAME, "prt-button-cover")
+            time.sleep(4)
+
+
+def join_raid(driver, raid_options, summons):
+    url_raid = "http://game.granbluefantasy.jp/#quest/assist"
+    load_wait(driver, url_raid)
+    click_by(driver, driver, By.ID, "tab-id")
+    
+    raid_id = get_raid_id(driver, raid_options)
+
+    get_element_by(driver, driver, By.CLASS_NAME, 
+            "frm-battle-key").send_keys(raid_id)
+    click_by(driver, driver, By.CLASS_NAME, "btn-post-key")
+    
+    time.sleep(2)
+    popups_visible = checkfor_popups(driver, popup_type="common-pop-error")
+
+    if len(popups_visible) != 0:
+        popup_txt = get_element_by(driver, popups_visible[0], By.CLASS_NAME,
+                "txt-popup-body")
+        if popup_txt.text == "Check your pending battles.":
+            clear_pending_battles(driver)
+
+        join_raid(driver, raid_options, summons)
+        return
+
+    time.sleep(3)
+
+    popups_visible = enter_quest(driver, driver.current_url, summons)
+
+    time.sleep(2)
+
+    if len(popups_visible) != 0:
+        join_raid(driver, raid_options, summons)
+        return
+
+    if "empty" in driver.current_url:
+        join_raid(driver, raid_options, summons)
+        return
+
+def raid_sequence(driver, raid_options, summons):
+    while True:
+        join_raid(driver, raid_options, summons)
+
+        wait_for_displayed(driver, By.CSS_SELECTOR, 'div[class="btn-attack-start display-on"]')
+
+        activate_skill(driver, 1, [4003])
+        time.sleep(7)
+        
--- raid.py
+++ raid.py
@@ -7,6 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 EMAIL = "xianhaic+1@gmail.com"
 PASS = "153968Pa"
 
@@ -16,27 +17,4 @@ options.add_argument("user-data-dir=/home/xianhai/.config/google-chrome");
 driver = webdriver.Chrome(chrome_options=options)
 driver.get("http://game.granbluefantasy.jp/#mypage")
 time.sleep(3)
-
-num_cycles = 0
-time_start = time.time()
-time_current = time_start
-print("Starting sequence: 1hr cycle, 2hr break (non-randomized)")
-while True:
-    print("Processing cycle #", num_cycles)
-    print("")
-
-    time_current = time.time()
-    
-    quest_sequence(driver, time_hrs=1.0)
-    
-    print("Cycle duration:", time.time() - time_current)
-    print("Total time elapsed:", time.time() - time_start)
-    time_current = time.time()
-    
-    print("")
-    
-    num_cycles += 1
-
-    print("Sleeping for 2hrs")
-    time.sleep(2 * 60 * 60)
-
+raid_sequence(driver, ["lvl75celesteomega"], [2040114000, 2040185000, 2040003000]) 
